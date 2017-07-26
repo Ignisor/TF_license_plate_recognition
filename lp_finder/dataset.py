af7978ba264cb9044bd769c5d8fd0e5fcb9ba954
ignis2497 <ignis2497@gmail.com>
2017-07-26 11:39:06 +0300

    generator for wrong images

--- lp_finder/dataset.py
+++ lp_finder/dataset.py
@@ -1,5 +1,7 @@
 import random
 import time
+from io import BytesIO
+from urllib.request import urlopen
 
 from PIL import Image, ImageDraw, ImageFont
 
@@ -8,39 +10,37 @@ from base import DataSetBase
 
 class LPDataset(DataSetBase):
     def _get_set(self, amount=None, test=False):
+        generators = (
+            (LPDataset.generate_lp, [1, 0]),
+            (LPDataset.generate_wrong_img, [0, 1])
+        )
+
         imgs = []
         labels = []
-        amount = amount or 1000
+        amount = amount or 100
 
         for i in range(amount):
-            img = LPDataset.generate_lp()
+            gen, label = random.choice(generators)
+            img = gen()
             img = LPDataset._process_image(img)
+
             imgs.append(img)
-            labels.append([1, 0])
+            labels.append(label)
 
         return imgs, labels
 
     @staticmethod
-    def generate_lp():
+    def generate_lp(text=None):
         """generate random license plate"""
-        rand_letter = lambda count: ''.join(random.choice('QWERTYUIOPASDFGHJKLZXCVBNM') for _ in range(count))
-        rand_num = lambda count: ''.join(str(random.randint(0, 9)) for _ in range(count))
-        text = f'{rand_letter(2)} {rand_num(4)} {rand_letter(2)}'
+        if not text:
+            rand_letter = lambda count: ''.join(random.choice('QWERTYUIOPASDFGHJKLZXCVBNM') for _ in range(count))
+            rand_num = lambda count: ''.join(str(random.randint(0, 9)) for _ in range(count))
+            text = f'{rand_letter(2)} {rand_num(4)} {rand_letter(2)}'
 
         img = Image.new('RGBA', (256, 64))
 
         # generate background
-        colors_amount = 20
-        back_colors = [LPDataset.get_random_color() for _ in range(colors_amount)]
-        n = 0
-        pixels = img.load()
-        for i in range(img.size[0]):
-            step = random.randint(1, 7)
-            for j in range(img.size[1]):
-                pixels[i, j] = back_colors[n]
-                n += step
-                if n > colors_amount - 1:
-                    n = 0
+        LPDataset.generate_noise(img)
 
         # generate plate
         plate = Image.new('RGB', (140, 30), LPDataset.get_random_color(235, 255))
@@ -70,7 +70,47 @@ class LPDataset(DataSetBase):
 
         img = Image.alpha_composite(img, n_plate)
 
-        del plate, n_plate
+        return img
+
+    @staticmethod
+    def generate_wrong_img():
+        """Generates random non LP image"""
+        generators = (
+            (LPDataset.generate_lp, ' ', ),  # Generate LP without text
+            (LPDataset.get_rand_img, (256, 64), ),
+            (LPDataset.generate_noise, Image.new('RGBA', (256, 64)), ),
+        )
+
+        gen = random.choice(generators)
+        return gen[0](*gen[1:])
+
+    @staticmethod
+    def get_rand_img(size):
+        """Gets random image using 'lorempixel.com'"""
+        url = f'http://lorempixel.com/{size[0]}/{size[1]}/'
+        file = BytesIO(urlopen(url).read())
+        image = Image.open(file)
+
+        return image
+
+    @staticmethod
+    def generate_noise(img, colors_amount=20):
+        """
+        Generate noise on image
+        :param PIL.Image.Image img: image to generate noise on
+        :param int colors_amount: amount of colors to use
+        """
+        back_colors = [LPDataset.get_random_color() for _ in range(colors_amount)]
+
+        n = 0
+        pixels = img.load()
+        for i in range(img.size[0]):
+            step = random.randint(1, 7)
+            for j in range(img.size[1]):
+                pixels[i, j] = back_colors[n]
+                n += step
+                if n > colors_amount - 1:
+                    n = 0
 
         return img
 
@@ -80,8 +120,6 @@ class LPDataset(DataSetBase):
         vector = []
         for pixel in img.getdata():
             vector.append((pixel[0] / 255, pixel[1] / 255, pixel[2] / 255))
-
-        del img
 
         return vector
 
