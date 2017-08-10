34eeee9d7c412eab7600e6cb7a9213ab969d9af5
ignis2497 <ignis2497@gmail.com>
2017-08-10 15:43:48 +0300

    video processing

--- base/video_processor.py
+++ base/video_processor.py
@@ -6,59 +6,87 @@ from PIL import Image, ImageDraw
 import numpy as np
 import cv2
 
-from ftplib import FTP
-
 
 class VideoProcessor(metaclass=ABCMeta):
+    SEARCH_RECTANGLE = ((400, 500), (1000, 700))
+
     def __init__(self, video_path, nn_class, part_ratio):
         self.nn = nn_class()
         self.part_ratio = part_ratio
 
+    def process_video(self, video_file, frameskip=5):
+        """
+        Split video in to frames
+        :param str video_file: path to video 
+        :param int frameskip: how many frames to skip
+        """
+        video = cv2.VideoCapture(video_file)
+        c = 0
+        while video.isOpened():
+            ret, frame = video.read()
+
+            d_frame = np.empty_like(frame)
+            d_frame[:] = frame
+            cv2.rectangle(d_frame, self.SEARCH_RECTANGLE[0], self.SEARCH_RECTANGLE[1], color=(0, 0, 255))
+
+            c += 1
+            if c >= frameskip:
+                plates = self.process_frame(frame)
+                c = 0
+
+                for plate in plates:
+                    cv2.imshow('plate', plate)
+                    while True:
+                        if cv2.waitKey(1) & 0xFF == ord('n'):
+                            break
+
+                cv2.imshow('frame', d_frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    cv2.destroyAllWindows()
+                    break
+
     def process_frame(self, frame, part_ratio=None, step=0.25):
         """
-        Split frame on many small images and send them to process_part
-        :param PIL.Image.Image frame: image with frame to process
+        Use provided NN for many parts in SEARCH_RECTANGLE
+        :param np.array frame: image with frame to process
         :param part_ratio: ratio of the frame parts
         :param float step: how much pixels skip per iteration
-        :return: data from process_part method
+        :return: positions of found LPs
         """
         def mul_range(start, end, step):
-            while start > end:
+            while start < end:
                 yield start
                 start = int(start * step)
 
         if not part_ratio:
             part_ratio = self.part_ratio
 
-        for height in mul_range(frame.height, 64, 1 - step):
+        # crop image
+        s_rect = self.SEARCH_RECTANGLE
+        img = frame[s_rect[0][1]:s_rect[1][1], s_rect[0][0]:s_rect[1][0]]
+
+        img_height, img_width = img.shape[:2]
+        img_center = (img_width // 2, img_height // 2)
+
+        parts_batch = []
+        t = time.time()
+        for height in mul_range(32, img_height, 1 + step):
             width = int(height * part_ratio)
 
-            t = time.time()
-            parts_batch = []
-            for x in range(0, frame.width, int(width * 0.33)):
-                for y in range(0, frame.height, int(height * 0.33)):
-                    part = frame.crop((x, y, x + width, y + height))
-                    part = part.resize(self.nn.INPUT_SIZE[1:3])
-                    parts_batch.append(np.array(part))
+            part_rect = (
+                (img_center[0] - width, img_center[1] - height),
+                (img_center[0] + width, img_center[1] + height),
+            )
 
-            logging.debug(f"parts batch got in: {time.time() - t:.4f}s")
+            part = img[part_rect[0][1]:part_rect[1][1], part_rect[0][0]:part_rect[1][0]]
+            part = cv2.resize(part, tuple(self.nn.INPUT_SIZE[1:3]))
 
-            results = self.nn.run_batch(parts_batch)
+            parts_batch.append(part)
 
-            all_imags = np.array([])
+        logging.debug(f"parts batch got in: {time.time() - t:.4f}s")
 
-            for part, result in zip(parts_batch, results):
-                if result:
-                    border = cv2.copyMakeBorder(part, top=1, bottom=1, left=1, right=1,
-                                                borderType=cv2.BORDER_CONSTANT,
-                                                value=[255, 0, 0])
+        results = self.nn.run_batch(parts_batch)
 
-                    all_imags = np.append(all_imags, border, axis=0) if all_imags.size > 0 else border
-
-            if all_imags.size > 0:
-                cv2.imshow('plate', cv2.cvtColor(all_imags, cv2.COLOR_BGR2RGB))
-
-                while True:
-                    if cv2.waitKey(25) & 0xFF == ord('q'):
-                        cv2.destroyAllWindows()
-                        break
+        for result, rect in zip(results, parts_batch):
+            if result:
+                yield rect
--- manage.py
+++ manage.py
@@ -87,17 +87,17 @@ class TempCmd(Command):
         logger = logging.getLogger()
         logger.setLevel(logging.DEBUG)
 
-        from PIL import Image
-        img = Image.open('/home/ignisor/Downloads/Telegram Desktop/cars.jpg')
-        from lp_finder import LPRecogniser
+        # from PIL import Image
+        # img = Image.open('/home/ignisor/Downloads/Telegram Desktop/cars.jpg')
 
+        from lp_finder import LPRecogniser
         from base import VideoProcessor
         vp = VideoProcessor('', LPRecogniser, 4 / 1)
 
         import time
         t = time.time()
-        result = vp.process_frame(img)
-        logging.debug(f"frame processed in: {time.time() - t:.4f}s")
+        result = vp.process_video('/home/ignisor/Downloads/pl111.avi')
+        logging.debug(f"video processed in: {time.time() - t:.4f}s")
 
 
 def all_subclasses(cls):
